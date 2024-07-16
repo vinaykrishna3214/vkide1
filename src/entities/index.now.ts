39c8361525a440ee1eff167675d2208e93374fa7
System Administrator <admin@example.com>
2024-07-16 15:03:28 +0530

    dfgv

--- src/entities/index.now.ts
+++ src/entities/index.now.ts
@@ -21,7 +21,7 @@ ClientScript({
     }`,
 })
 
-//creates a business rule that pops up state change message whenever a todo record is updated
+//creates a business rule that pops uppp state change message whenever a todo record is updated
 BusinessRule({
     $id: Now.ID['br0'],
     action: ['update'],
